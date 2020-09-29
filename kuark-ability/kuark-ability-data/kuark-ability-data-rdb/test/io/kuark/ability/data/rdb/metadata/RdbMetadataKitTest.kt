b8f816a58e741fddf652a10d7f06cd92f2270b18
K <19840104@qq.com>
2020-09-29 22:26:10 +0800

    yml

---
+++ kuark-ability/kuark-ability-data/kuark-ability-data-rdb/test/io/kuark/ability/data/rdb/metadata/RdbMetadataKitTest.kt
@@ -0,0 +1,23 @@
+package io.kuark.ability.data.rdb.metadata
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class RdbMetadataKitTest {
+
+    @Test
+    fun getTablesByType() {
+        TODO()
+    }
+
+    @Test
+    fun getTableByName() {
+        TODO()
+    }
+
+    @Test
+    fun getColumnsByTableName() {
+        TODO()
+    }
+}
---
+++ kuark-ability/kuark-ability-data/kuark-ability-data-rdb/test/io/kuark/ability/data/rdb/support/RdbKitTest.kt
@@ -0,0 +1,39 @@
+package io.kuark.ability.data.rdb.support
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class RdbKitTest {
+
+    @Test
+    fun getDataSource() {
+        RdbKit.getDataSource()
+        TODO()
+    }
+
+    @Test
+    fun getDatabase() {
+        TODO()
+    }
+
+    @Test
+    fun newConnection() {
+        TODO()
+    }
+
+    @Test
+    fun testConnection() {
+        TODO()
+    }
+
+    @Test
+    fun determinRdbTypeByUrl() {
+        TODO()
+    }
+
+    @Test
+    fun getTestStatement() {
+        TODO()
+    }
+}

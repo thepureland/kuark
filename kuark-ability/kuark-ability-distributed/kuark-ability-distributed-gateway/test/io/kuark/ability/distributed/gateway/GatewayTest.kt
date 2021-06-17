4dd333274082461d1f037b3c023542923447fe80
k <19840104@qq.com>
2021-06-17 21:11:02 +0800

    add gateway test

--- kuark-ability/kuark-ability-distributed/kuark-ability-distributed-gateway/test/io/kuark/ability/distributed/gateway/GatewayTest.kt
+++ kuark-ability/kuark-ability-distributed/kuark-ability-distributed-gateway/test/io/kuark/ability/distributed/gateway/GatewayTest.kt
@@ -1,6 +1,7 @@
 package io.kuark.ability.distributed.gateway
 
 import io.kuark.base.lang.SystemKit
+import io.kuark.base.net.http.HttpClientKit
 import io.kuark.test.server.eureka.EurekaServer
 import io.kuark.test.service.service1.Service1Node1Application
 import io.kuark.test.service.service1.Service1Node2Application
@@ -8,6 +9,10 @@ import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -15,10 +20,12 @@ internal open class GatewayTest {
 
     @BeforeAll
     fun setUp() {
-        SystemKit.setEnvVars(mapOf(
-            "eureka.client.service-url.defaultZone" to "http://localhost:8001/eureka/",
-            "spring.cloud.gateway.enabled" to "false"
-        ))
+        SystemKit.setEnvVars(
+            mapOf(
+                "eureka.client.service-url.defaultZone" to "http://localhost:8001/eureka/",
+                "spring.cloud.gateway.enabled" to "false"
+            )
+        )
 
         EurekaServer.main(arrayOf())
         Service1Node1Application.main(arrayOf())
@@ -36,7 +43,13 @@ internal open class GatewayTest {
 
     @Test
     fun getTablesByType() {
-        println("getTablesByType")
+        val url = "http://localhost:9201/service1/list"
+        val httpClientBuilder = HttpClient.newBuilder()
+        val httpRequestBuilder = HttpRequest.newBuilder().uri(URI.create(url))
+        val response =
+            HttpClientKit.request(httpClientBuilder, httpRequestBuilder, HttpResponse.BodyHandlers.ofString())
+        val body = response.body()
+        println(body)
     }
 
 }
--- kuark-test/src/io/kuark/test/service/service1/biz/Biz1.kt
+++ kuark-test/src/io/kuark/test/service/service1/biz/Biz1.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service
 class Biz1 : IBiz1 {
 
     override fun list(): List<Result1> {
-        return listOf(Result1("1", "name1"), Result1("1", "name1"))
+        return listOf(Result1("1", "name1"), Result1("2", "name2"))
     }
 
 }
